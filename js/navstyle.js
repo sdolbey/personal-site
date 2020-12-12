37be8e74d3a0f6eedf1b20021bb4d0c43bca7b8b
sdolbey <steven.dolbey@gmail.com>
2020-12-12 18:37:25 -0500

    removed obsolete code

--- js/navstyle.js
+++ js/navstyle.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //let scroll = window.scrollY;
     const navbar = document.querySelector(".navbar");
-    //console.log(navbar);
-    //const navbarHeight = navbar.offsetHeight;
     
     const add_class = () => navbar.classList.add("scrolled");
     const remove_class = () => navbar.classList.remove("scrolled");
